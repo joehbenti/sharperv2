9c38e2437d007fb6caed2e77c8fe7ef088266a5c
Jote Hailu <joehbenti@gmail.com>
2019-09-09 16:25:31 +0300

    minor refactorin

--- client/src/component/home.js
+++ client/src/component/home.js
@@ -11,37 +11,9 @@ import { Row, Col, Button } from 'react-bootstrap';
 
 class Home extends Component {
 	render() {
-		const h1Style = {
-			fontSize: '4em'
-		};
-
-		const h3Style = {
-			fontFamily: 'Quicksand',
-			fontSize: '3em',
-			color: 'white',
-			marginBottom: '60px'
-		};
-
-		const mainText = {
-			color: '#FFD000',
-			fontFamily: 'Indie Flower'
-		};
-
 		const paddingDiv = {
 			marginTop: '50px'
 		};
-
-		const listStyle = {
-			listStyle: 'none',
-			display: 'inline-block',
-			color: 'white',
-			fontSize: '1.5em',
-			paddingLeft: '0'
-		};
-
-		// const liStyle = {
-		// 	display:
-		// }
 
 		const BounceLeft = styled.div`
 			animation: 1.5s ${keyframes`${bounceInLeft}`};
@@ -57,24 +29,26 @@ class Home extends Component {
 					<BounceLeft>
 						<Row>
 							<Col lg='7'>
-								<h1 style={h1Style}>
+								<h1 className='h1Style'>
 									Front-End Developer <br /> & Brand Consultant
 								</h1>
 								<br />
 								<br />
-								<h3 style={h3Style}>
-									I am a self-thought{' '}
-									<span style={mainText}>multi-disciplinary</span> designer with
-									a focus for{' '}
-									<span style={mainText}>human centered design.</span>{' '}
+								<h3 className='h3Style'>
+									I am a pro-active self-thought{' '}
+									<span className='mainText'>multi-disciplinary</span> designer
+									with a focus for{' '}
+									<span className='mainText'>human centered design.</span>{' '}
 								</h3>
 								<div>
 									<h3>Services</h3>
 									<BounceService>
 										<p>
 											{/* <ul style={listStyle}> */}
-											<li style={listStyle}>
-												Website Dev, Android Dev, Consulting & Branding
+											<li className='listStyle'>
+												<span className='services'>Branding</span>
+												<span className='services'>Web/Mobile Dev</span>
+												<span className='services'>Graphics Design</span>
 											</li>
 											{/* </ul> */}
 										</p>
@@ -83,6 +57,9 @@ class Home extends Component {
 								<div style={paddingDiv}>
 									<Button variant='outline-warning' size='lg'>
 										<Link to='/contact' className='contactLink'>
+											<span role='img' arial-role='click me'>
+												👉
+											</span>{' '}
 											Let's work together
 										</Link>
 									</Button>
